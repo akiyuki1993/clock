88446db3dcf9e02a31b72ae7ef1151a77bbbfdcb
akiyuki1993 <yamadaakiyuki@yamada-no-MacBook-Air.local>
2015-06-16 16:47:56 +0900

    refs #1 長針の色を変更するオプションを追加した。
    変更した

--- src/main/java/com/github/clock/swing/ClockViewer.java
+++ src/main/java/com/github/clock/swing/ClockViewer.java
@@ -7,7 +7,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import github.args;
+import com.github.clock.Args;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
@@ -28,8 +28,11 @@ public class ClockViewer extends JComponent{
     private Clock clock;
     private Image background;
     private boolean debugMode = false;
+    private Args args;
 
     public ClockViewer(Clock clock){
+        public ClockViewer(Clock clock, Args args){       
+        this.args = args;
         clock.addUpdateObserver(new UpdateObserver(){
             @Override
             public void update(Clock clock) {
@@ -119,7 +122,7 @@ public class ClockViewer extends JComponent{
 
         g2.setStroke(new BasicStroke(2));
         g2.setColor(Color.RED);
-        g2.setColor()
+        g2.setColor(Color.decode(args.getLongHandColor()));
         drawHand(g2, clock.getMinute(), length * 0.7);
         // 長針の位置は，分の位置で表そうとすると，時間×5．
         // 24時間制のため，12で割った余りを時間とする．
